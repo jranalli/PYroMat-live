aac86766fd87d9d122279ba994cbe12706613e6e
Joe Ranalli <jar339@psu.edu>
2022-03-16 14:07:48 -0400

    Simplest possible pyromat

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import pyromat as pm
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    s = pm.get('mp.H2O')
+    return "The temperature is T={} K.".format(s.Ts(p=1))
 
 
 if __name__ == '__main__':
