a4fc75bfe8dbdfc2dae2f5cf2b20879caa6aac3a
Joe Ranalli <jar339@psu.edu>
2022-12-21 16:18:43 -0500

    Cleanup execution of the isoline calculations

--- static/cookies.js
+++ static/cookies.js
@@ -1,22 +1,116 @@
+const DEFAULT_IDSTR = "mp.H2O";
+const DEFAULT_UNITS = {
+    energy: "kJ",
+    force: "N",
+    length: "m",
+    mass: "kg",
+    matter: "kg",
+    molar: "kmol",
+    pressure: "bar",
+    temperature: "K",
+    time: "s",
+    volume: "m3"
+};
+const SUBST_COOKIE_ID = "idstr";
+const UNITS_COOKIE_ID = "units";
 
-//**************
-// Cookie management functions
-//**************
 
-// Set a cookie value
-//      set_cookie(param,value)
-// This assigns a value to the string parameter name, param.  The cookie
-// expiration is not set.
+// *****************************************************************
+// * PYroMat Cookie Management Functions
+// *    Functions for setting and getting cookies germane to common PYroMat
+// *    tasks.
+// *****************************************************************
+
+
+
+/**
+ * Load the substance id from a cookie
+ * @returns {*|string} - the id string (e.g. mp.H2O)
+ */
+function load_substance_cookie(){
+    let sub = get_cookie(SUBST_COOKIE_ID);
+
+    // If not set, initialize
+    if (sub === "") {
+        sub = DEFAULT_IDSTR;
+        set_substance_cookie(sub);
+    }
+    return sub;
+}
+
+
+/**
+ * Load the units configuration from a cookie
+ * @returns dict - the units as a dictionary
+ */
+function load_units_cookie() {
+    let actual = get_cookie(UNITS_COOKIE_ID);
+
+    // If the unit data isn't set, initialize it.
+    if (actual === "") {
+        actual = DEFAULT_UNITS;
+        set_units_cookie(actual);
+    } else {
+        // Cookie is stringified, so we need to parse it out
+        actual = JSON.parse(actual);
+    }
+    return actual;
+}
+
+
+/**
+ * Set the substance cookie using an id string
+ * @param substance - str, id string (e.g. mp.H2O)
+ */
+function set_substance_cookie(substance){
+    set_cookie(SUBST_COOKIE_ID, substance);
+}
+
+
+/**
+ * sSet the units cookie using a dict of units
+ * @param units_dict - dict, keyed by unit (see DEFAULT_UNITS) for example.
+ */
+function set_units_cookie(units_dict){
+    set_cookie(UNITS_COOKIE_ID, JSON.stringify(units_dict));
+}
+
+
+// *****************************************************************
+// * Generic Cookie Management Functions
+// *    Functions for setting and getting cookies without any specificity to
+// *    the PYroMat library.
+// *****************************************************************
+
+
+/**
+ * Set a cookie value
+ *      set_cookie(param,value)
+ *
+ * This assigns a value to the string parameter name, param.  The cookie
+ * expiration is set to one hour.
+ *
+ * @param param - str, name for the cookie
+ * @param value - any, the value to store
+ */
 function set_cookie(param, value){
     // Set the cookie to expire in one hour
     set_cookie_exp(param, value,3600000);
 }
 
-// Set a cookie with an expiration value
-//      set_cookie_exp(param,value,exp)
-// This assigns a value to the string parameter name, param.  The cookie
-// expiration is set to exp milliseconds from now.  To delete a cookie,
-// pass a negative value to exp
+
+/**
+ * Set a cookie value
+ *      set_cookie_exp(param, value, exp)
+ *
+ * This assigns a value to the string parameter name, param.  The cookie
+ * expiration is set to one hour. To delete a cookie, pass a negative value to
+ * exp.
+ *
+ * @param param - str, name for the cookie
+ * @param value - any, the value to store
+ * @param exp - int, the expiration in milliseconds
+ */
 function set_cookie_exp(param, value, exp){
     // Set the cookie to expire in one hour
     time = new Date();
@@ -26,11 +120,18 @@ function set_cookie_exp(param, value, exp){
 
 }
 
-// GET_COOKIE
-//      get_cookie(param)
-// Recovers the value associated with a cookie with the string name,
-// param.  If no cookie with a matching param name is found, an empty
-// string is returned.
+
+/**
+ * Get value for a cookie by name
+ *      get_cookie(param)
+ *
+ * Recovers the value associated with a cookie with the string name,
+ * param.  If no cookie with a matching param name is found, an empty
+ * string is returned.
+ *
+ * @param param - str, name for the cookie
+ * @returns {string|string|jQuery|*|string} - the value from the cookie
+ */
 function get_cookie(param){
     let pairs = decodeURIComponent(document.cookie).split(';');
     let tparam, declare, value;
--- static/pointcalc.js
+++ static/pointcalc.js
@@ -1,22 +1,26 @@
-var DEFAULT_IDSTR = "mp.H2O";
+// Global storage of the PYroMat data
 var infodata;
 
+// Models
 var unitModel;
 var dataModel;
 
-var unitPickerView;
+// User Controls
+var propEntryForm;
+var tableControlsModal;
+var substancePickerModal;
+var unitControlsModal
+var plotControlsModal;
 
-var propEntryView;
-var propChooserView;
+// Views
 var plotView;
 var tableView;
 
-var substancePicker;
 
-
-var plotControls;
-
-// document.ready()
+/**
+ * Equivalent to document.ready(). Run when the page loads. Begin
+ * initialization by loading the general information.
+ */
 $(function(){
 
     // infodata holds the basic info about PYroMat substances.
@@ -35,114 +39,101 @@ $(function(){
     }
 });
 
+
+/**
+ * Execute the initialization of the page. Requires that infodata has already
+ * been initialized.
+ */
 function init(){
-    // Infodata has now been initialized
+    if (!infodata){
+        alert("Error in page initialization. Please report as a bug!")
+    }
+    // Infodata is definitely initialized
 
-    let subid = load_substance_choice();
-    let units = load_units_choice();
+    // Load the substance and units from the cookies
+    let subid = load_substance_cookie();
+    let units = load_units_cookie();
 
-    $("#substance_title").text("Substance: "+subid);
+    // Initialize the substance display text
+    display_substance(subid);
 
+    // Create the models that will hold onto the data
     dataModel = new DataModel(subid);
     unitModel = new UnitModel(infodata.data.legalunits, units);
 
-    substancePicker = new ModalSubstancePicker('modal_substancepicker',
+    substancePickerModal = new ModalSubstancePicker('modal_substancepicker',
         '../static/modal_substance.html',
         infodata.data.substances);
 
-    unitPickerView = new UnitFormView($('#modal_unitspicker'),
+    unitControlsModal = new UnitFormView($('#modal_unitspicker'),
         "../static/unitspicker.html",
         infodata.data.legalunits,
         units,
         infodata.units,
-        apply_units,
-        onclick_changeunits);
+        change_units,
+        onclick_toggle_unitcontrols);
 
 
     calculate_plot_isolines();
 
-    propEntryView = new PropEntryView("property_controls",
+    propEntryForm = new PropEntryView("property_controls",
         dataModel.get_input_properties(),
         unitModel.get_units_for_prop(dataModel.get_input_properties()),
         compute_point);
 
 
-    //propChooserView = new PropChooserView($("#property_selection_outer"), PropChooserView.EVENT_PROPERTY_VISIBILITY);
-    propChooserView = new TableControls($("#property_selection_outer"), "../static/table_options.html", start_hidden=true);
 
-    plotControls = new PlotControls($("#plot_controls"), "../static/plot_options.html", true);
+    tableControlsModal = new TableControls($("#property_selection_outer"), "../static/table_options.html", start_hidden=true);
+
+    plotControlsModal = new PlotControls($("#plot_controls"), "../static/plot_options.html", true);
 
     plotView = new PlotView("plot_display", dataModel, unitModel.get_units_for_prop, compute_point);
     dataModel.addListener(plotView);
-    propChooserView.addListener(plotView);
-    plotControls.addListener(plotView);
+    tableControlsModal.addListener(plotView);
+    plotControlsModal.addListener(plotView);
     plotView.init();
 
     // Assign views to listen to the main model
     tableView = new TableView('property_table', dataModel, unitModel.get_units_for_prop);
     dataModel.addListener(tableView);
-    propChooserView.addListener(tableView);
+    tableControlsModal.addListener(tableView);
     tableView.init(dataModel.get_output_properties());
 
-    propChooserView.init(dataModel.get_output_properties());
-    plotControls.init(dataModel.get_output_properties());
+    tableControlsModal.init(dataModel.get_output_properties());
+    plotControlsModal.init(dataModel.get_output_properties());
 
 
 }
 
-function load_substance_choice(){
-    let sub = get_cookie("idstr");
-    if (sub === ""){
-        change_substance(DEFAULT_IDSTR);
-    } else {
-        display_substance(sub);
-    }
-    return sub;
-}
-function load_units_choice() {
-    let actual = get_cookie("units");
 
-    // If the unit data isn't set, initialize it.
-    if (actual === "") {
-        change_units(infodata.units);
-    } else {
-        actual = JSON.parse(actual);
-        display_units(actual);
-    }
-    return actual;
+
+
+
+function onclick_toggle_substancecontrols(){
+    substancePickerModal.toggle();
 }
 
 
-function apply_units(units){
-    unitPickerView.toggle();
-    change_units(units)
+function onclick_toggle_unitcontrols(){
+    unitControlsModal.toggle();
 }
 
-function onclick_changesubstance(){
-    substancePicker.toggle();
+function onclick_toggle_plotcontrols(){
+    plotControlsModal.toggle();
 }
 
-
-function onclick_changeunits(){
-    unitPickerView.toggle();
-}
-
-function onclick_showplotcontrols(){
-    plotControls.toggle();
-}
-
-function onclick_tableprop_checks(){
-    propChooserView.toggle();
+function onclick_toggle_tablecontrols(){
+    tableControlsModal.toggle();
 }
 
 function change_substance(substance){
-    set_cookie("idstr", substance);
+    set_substance_cookie(substance);
     location.reload();
 }
 
 function change_units(units){
-    set_cookie("units", JSON.stringify(units));
-    location.reload();
+    set_units_cookie(units)
+    location.reload()
 }
 
 function display_substance(sub){
@@ -155,35 +146,10 @@ function display_substance(sub){
     $("#sub_string").text(label);
 }
 
-function display_units(units){
-    $("#unit_string").text(JSON.stringify(units));
-}
-
-
-
-
-
-
-
 function calculate_plot_isolines(){
     if (dataModel.get_substance().startsWith('mp')){
-        compute_steamdome((data)=>{
-            let sll = data.data['liquid'];
-            let svl = data.data['vapor'];
-            // concatenate vapor to liquid
-            Object.keys(svl).forEach(key => {
-                for (let i = svl[key].length; i > -1; i--) {
-                    sll[key].push(svl[key][i]);
-                }
-            });
-            dataModel.add_auxline('steamdome', sll, parent='global');
-        });
-
-        compute_auxline((data)=>{
-            data.data.forEach((line)=>{
-                dataModel.add_auxline('x', line, 'global');
-            });
-        },{'x': 0, 'default': true});
+        compute_steamdome();
+        compute_auxline({'x': 0, 'default': true});
     }
 
     // Add a few types of lines
@@ -191,36 +157,63 @@ function calculate_plot_isolines(){
         compute_args = {};
         compute_args[prop_val] = 0;
         compute_args["default"] = true;
-        compute_auxline((response)=>{
-            response.data.forEach((line)=>{
-                dataModel.add_auxline(prop_val, line, 'global');
-            });
-        },compute_args);
-    })
+        compute_auxline(compute_args);
+    });
 }
 
 
-
 /**
- * Async request for getting a state from the backend.
- * @param callback - function handle to execute when complete
+ * Wrapper for computing isolines. Automatically adds result as an
+ * auxline to datamodel.
  * @param props - Dict with keys of property and numeric values
  */
-function compute_auxline(callback, props={}){
+function compute_auxline(props={}){
     ajax_isoline(dataModel.get_substance(),
         props,
         unitModel.get_units(),
         (response) => {
-        callback(response);
+            // Get the property that this line was for (maybe remove default)
+            let keys = Object.keys(props);
+            if (keys.indexOf("default") > -1){
+                keys.splice(keys.indexOf("default", 1))
+            }
+            let prop = keys[0];
+
+            // If it had multiple lines, loop over to add each individually
+            if (Array.isArray(response.data)){
+                response.data.forEach((line)=> {
+                    dataModel.add_auxline(prop, line, 'global');
+                });
+            } else {
+                // Add the line
+                dataModel.add_auxline(prop, line, 'global');
+            }
+
     });
 }
 
-function compute_steamdome(callback){
+/**
+ * Wrapper for computing the steamdome isoline. Automatically adds as an
+ * auxline to datamodel.
+ */
+function compute_steamdome(){
     ajax_saturation(dataModel.get_substance(),
         {},
         unitModel.get_units(),
         (response) => {
-        callback(response);
+            // We get separate liquid and vapor lines back
+            let sll = response.data['liquid'];
+            let svl = response.data['vapor'];
+
+            // concatenate vapor onto liquid so we have a single line
+            Object.keys(svl).forEach(key => {
+                for (let i = svl[key].length; i > -1; i--) {
+                    sll[key].push(svl[key][i]);
+                }
+            });
+
+            // Add the line to the model
+            dataModel.add_auxline('steamdome', sll,parent='global');
     });
 }
 
